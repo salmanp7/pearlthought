a0be12fee75510920f57cbe976f9174d6728aef6
salmanp7 <152171010+salmanp7@users.noreply.github.com>
2024-06-09 15:40:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_ecs_task_definition" "pearlthoughts" {
   family                   = "hello-world-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "1"
-  memory                   = "3"
+  cpu                      = "256"  
+  memory                   = "512"  
 
   container_definitions = jsonencode([
     {
