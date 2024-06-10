bdd38df5698495dcf1d417579c72ad8daeac99e0
salmanp7 <152171010+salmanp7@users.noreply.github.com>
2024-06-10 11:18:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_ecs_task_definition" "hello-world-task" {
   family                   = "hello-world-task"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "1024"
+  memory                   = "3072"
 
   container_definitions = jsonencode([
     {
