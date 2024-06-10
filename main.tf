202a7716655ed80385b3a881b54e3743bb77caf6
salmanp7 <152171010+salmanp7@users.noreply.github.com>
2024-06-10 12:06:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,28 +50,6 @@ resource "aws_security_group" "pearl" {
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
 resource "aws_ecs_cluster" "pearlthoughts-clusters" {
   name = "pearlthoughts-clusters"
 }
@@ -82,7 +60,7 @@ resource "aws_ecs_task_definition" "hello-world-task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "1024"
   memory                   = "3072"
-  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = "arn:aws:iam::<account-id>:role/ecsTaskExecutionRole" # Replace <account-id> with your AWS account ID
 
   container_definitions = jsonencode([
     {
