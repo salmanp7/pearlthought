f53243b28796d18bd1f61997d9f0007bfd4492d3
salmanp7 <152171010+salmanp7@users.noreply.github.com>
2024-06-10 12:00:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,13 +3,33 @@ provider "aws" {
 }
 
 resource "aws_vpc" "pearlthoughts-vpc" {
-  cidr_block = "172.31.0.0/16"
+  cidr_block           = "172.31.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 }
 
 resource "aws_subnet" "pearl" {
   vpc_id            = aws_vpc.pearlthoughts-vpc.id
   cidr_block        = "172.31.32.0/20"
   availability_zone = "ap-south-1a"
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.pearlthoughts-vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.pearlthoughts-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.pearl.id
+  route_table_id = aws_route_table.rt.id
 }
 
 resource "aws_security_group" "pearl" {
@@ -30,6 +50,28 @@ resource "aws_security_group" "pearl" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_cluster" "pearlthoughts-clusters" {
   name = "pearlthoughts-clusters"
 }
@@ -40,6 +82,7 @@ resource "aws_ecs_task_definition" "hello-world-task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "1024"
   memory                   = "3072"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
   container_definitions = jsonencode([
     {
